cb857456e58ebc7b988e18952b770cd61cfa9dea
Whitos <pavel.tsanev@etu.univ-smb.fr>
2025-12-11 08:08:59 +0100

    feat(di): register new services and sort alphabetically
    - Add missing service registrations (Article, Event, Order, Discovery, CelestialBody, etc.)
    - Re-enable EventInterestService
    - Sort all AddScoped declarations alphabetically for better readability and maintenance

--- Astralis_BlazorApp/Extensions/ServiceCollectionExtensions.cs
+++ Astralis_BlazorApp/Extensions/ServiceCollectionExtensions.cs
@@ -7,33 +7,48 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddScoped<IUserService, UserService>();
-        services.AddScoped<IUserNotificationService, UserNotificationService>();
         services.AddScoped<IUserRoleService, UserRoleService>();
+        services.AddScoped<IUserNotificationService, UserNotificationService>();
+        services.AddScoped<IUserNotificationTypeService, UserNotificationTypeService>();
+        services.AddScoped<IUserService, UserService>();
         services.AddScoped<ITypeOfArticleService, TypeOfArticleService>();
         services.AddScoped<IStarService, StarService>();
         services.AddScoped<ISpectralClassService, SpectralClassService>();
         services.AddScoped<ISatelliteService, SatelliteService>();
-        services.AddScoped<IReportService, ReportService>();
+        services.AddScoped<IReportStatusService, ReportStatusService>();
         services.AddScoped<IReportMotiveService, ReportMotiveService>();
+        services.AddScoped<IReportService, ReportService>();
         services.AddScoped<IProductCategoryService, ProductCategoryService>();
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<IPlanetTypeService, PlanetTypeService>();
         services.AddScoped<IPlanetService, PlanetService>();
         services.AddScoped<IPhonePrefixService, PhonePrefixService>();
+        services.AddScoped<IOrderDetailService, OrderDetailService>();
+        services.AddScoped<IOrbitalClassService, OrbitalClassService>();
         services.AddScoped<INotificationTypeService, NotificationTypeService>();
         services.AddScoped<INotificationService, NotificationService>();
         services.AddScoped<IGalaxyQuasarClassService, GalaxyQuasarClassService>();
+        services.AddScoped<IGalaxyQuasarService, GalaxyQuasarService>();
         services.AddScoped<IEventTypeService, EventTypeService>();
-        // services.AddScoped<IEventInterestService, EventInterestService>();
+        services.AddScoped<IEventInterestService, EventInterestService>();
+        services.AddScoped<IEventService, EventService>();
         services.AddScoped<IDiscoveryStatusService, DiscoveryStatusService>();
+        services.AddScoped<IDiscoveryService, DiscoveryService>();
         services.AddScoped<IDetectionMethodService, DetectionMethodService>();
         services.AddScoped<ICountryService, CountryService>();
         services.AddScoped<ICommentService, CommentService>();
         services.AddScoped<ICommandStatusService, CommandStatusService>();
+        services.AddScoped<ICommandService, CommandService>();
+        services.AddScoped<ICometService, CometService>();
+        services.AddScoped<ICityService, CityService>();
         services.AddScoped<ICelestialBodyTypeService, CelestialBodyTypeService>();
+        services.AddScoped<ICelestialBodyService, CelestialBodyService>();
+        services.AddScoped<ICartItemService, CartItemService>();
         services.AddScoped<IAudioService, AudioService>();
+        services.AddScoped<IAsteroidService, AsteroidService>();
         services.AddScoped<IArticleTypeService, ArticleTypeService>();
+        services.AddScoped<IArticleInterestService, ArticleInterestService>();
+        services.AddScoped<IArticleService, ArticleService>();
         services.AddScoped<IAliasStatusService, AliasStatusService>();
         services.AddScoped<IAddressService, AddressService>();
 
