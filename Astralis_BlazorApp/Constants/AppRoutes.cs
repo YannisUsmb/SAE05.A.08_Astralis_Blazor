b3ea5dbfb24388b55b891ac48e3af723a1e5dd25
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-17 10:21:55 +0100

    refactor(pages) : add AppRoutes to handle name changements

---
+++ Astralis_BlazorApp/Constants/AppRoutes.cs
@@ -0,0 +1,17 @@
+﻿namespace Astralis_BlazorApp.Constants
+{
+    public static class AppRoutes
+    {
+        public const string Home = "/";
+
+        public const string Articles = "/articles";
+        public const string Audios = "/audios";
+        public const string Connexion = "/connexion";
+        public const string CelestialBodies = "corps-celestes";
+        public const string Events = "/evenements";
+        public const string SignUp = "/inscription";
+        public const string Profile = "/profil";
+        public const string Premium = "/premium";
+        public const string Shop = "/boutique";
+    }
+}
